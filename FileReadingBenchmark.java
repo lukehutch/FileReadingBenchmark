420fc222a235e88096398772ab1e62f427414052
Luke Hutchison <luke.hutch@gmail.com>
2018-08-08 04:02:10 -0600

    Make compatible with JDK 8

--- FileReadingBenchmark.java
+++ FileReadingBenchmark.java
@@ -1,3 +1,4 @@
+import java.nio.file.Files;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,7 +9,9 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -23,6 +26,70 @@ public class FileReadingBenchmark {
      */
     private static final int TOT_BYTES = 102_400_000;
     private static final int FILES_PER_DIR = 1000;
+
+    /** The default size of a file buffer. */
+    private static final int DEFAULT_BUFFER_SIZE = 16384;
+
+    /**
+     * The maximum size of a file buffer array. Eight bytes smaller than {@link Integer.MAX_VALUE}, since some VMs
+     * reserve header words in arrays.
+     */
+    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
+
+    /** The maximum initial buffer size. */
+    private static final int MAX_INITIAL_BUFFER_SIZE = 16 * 1024 * 1024;
+
+    /**
+     * Read all the bytes in an {@link InputStream}.
+     * 
+     * @param inputStream
+     *            The {@link InputStream}.
+     * @param fileSizeHint
+     *            The file size, if known, otherwise -1L.
+     * @return The contents of the {@link InputStream} as an Entry consisting of the byte array and number of bytes
+     *         used in the array..
+     * @throws IOException
+     *             If the contents could not be read.
+     */
+    private static SimpleEntry<byte[], Integer> readAllBytes(
+                final InputStream inputStream, final long fileSizeHint) throws IOException {
+        if (fileSizeHint > MAX_BUFFER_SIZE) {
+            throw new IOException("InputStream is too large to read");
+        }
+        final int bufferSize = fileSizeHint < 1L
+                // If fileSizeHint is unknown, use default buffer size 
+                ? DEFAULT_BUFFER_SIZE
+                // fileSizeHint is just a hint -- limit the max allocated buffer size, so that invalid ZipEntry
+                // lengths do not become a memory allocation attack vector
+                : Math.min((int) fileSizeHint, MAX_INITIAL_BUFFER_SIZE);
+        byte[] buf = new byte[bufferSize];
+
+        int bufLength = buf.length;
+        int totBytesRead = 0;
+        for (int bytesRead;;) {
+            // Fill buffer -- may fill more or fewer bytes than buffer size
+            while ((bytesRead = inputStream.read(buf, totBytesRead, bufLength - totBytesRead)) > 0) {
+                totBytesRead += bytesRead;
+            }
+            if (bytesRead < 0) {
+                // Reached end of stream
+                break;
+            }
+            // bytesRead == 0 => grow buffer, avoiding overflow
+            if (bufLength <= MAX_BUFFER_SIZE - bufLength) {
+                bufLength = bufLength << 1;
+            } else {
+                if (bufLength == MAX_BUFFER_SIZE) {
+                    throw new IOException("InputStream too large to read");
+                }
+                bufLength = MAX_BUFFER_SIZE;
+            }
+            buf = Arrays.copyOf(buf, bufLength);
+        }
+        // Return buffer and number of bytes read
+        return new SimpleEntry<>((bufLength == totBytesRead) ? buf : Arrays.copyOf(buf, totBytesRead),
+                totBytesRead);
+    }
 
     public static void main(String[] args) throws IOException {
         System.out.println("Filesize\tNumFiles\t|\tInputStream1\tInputStream2\tInputStream4\tInputStream8\t|\t"
@@ -68,8 +135,8 @@ public class FileReadingBenchmark {
                     for (ExecutorService threadPool : threadPools) {
                         long t1 = System.nanoTime();
                         filesToRead.stream().map(f -> threadPool.submit(() -> {
-                            try (InputStream is = new FileInputStream(f)) {
-                                is.readAllBytes();
+                            try (InputStream is = Files.newInputStream(f.toPath())) {
+                                readAllBytes(is, f.length());
                             }
                             return null;
                         })).forEach(f -> {
